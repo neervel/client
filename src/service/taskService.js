70ba408922076cfc61cd057825d8d61a1dd55d06
Neervel <klihopoy@mail.ru>
2021-08-01 12:59:21 +0300

    add task service

--- src/service/taskService.js
+++ src/service/taskService.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "https://boiling-escarpment-95429.herokuapp.com/api/tasks"
+const BASE_URL = "https://boiling-escarpment-95429.herokuapp.com/api/tasks/"
 
 class taskService {
   async getAll () {
@@ -13,6 +13,14 @@ class taskService {
 
   async create(task) {
     await axios.post(BASE_URL, task)
+  }
+
+  async delete(id) {
+    await axios.delete(BASE_URL+id)
+  }
+
+  async edit(task) {
+    await axios.put(BASE_URL+task.id, task)
   }
 }
 
